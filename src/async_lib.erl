e5c09700eb02d44656838dbb875b9d774fbeca0e
platinumthinker <platinumthinker@gmail.com>
2017-04-09 20:10:37 +0700

    Added function getting real path for symlinks (for rebar3).

--- src/async_lib.erl
+++ src/async_lib.erl
@@ -3,7 +3,8 @@
 -export([
          chain/2,
          env/1,
-         env/2
+         env/2,
+         get_real_directory/1
         ]).
 
 -export_type([
@@ -26,3 +27,18 @@ env(Prop) -> env(Prop, undefined).
 -spec env(atom(), any()) -> any().
 env(Prop, Default) ->
     application:get_env(async, Prop, Default).
+
+-spec get_real_directory(file:filename()) -> file:filename().
+get_real_directory(Dir) ->
+    case file:read_link(Dir) of
+        {ok, OtherDir} ->
+            Dir1 = case filename:pathtype(OtherDir) of
+                relative ->
+                    filename:absname_join(filename:dirname(Dir), OtherDir);
+                _ ->
+                    OtherDir
+            end,
+            get_real_directory(Dir1);
+        {error, _} ->
+            Dir
+    end.
