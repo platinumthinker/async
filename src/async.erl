5988d02cbf6d2466ba8432f91ffb5a39263da34b
platinumthinker <platinumthinker@gmail.com>
2017-04-11 00:43:04 +0700

    Add missing file

---
+++ src/async.erl
@@ -0,0 +1,19 @@
+-module(async).
+
+-export([
+         watch/1,
+         unwatch/1,
+         pause/0,
+         unpause/0
+        ]).
+
+-spec watch(Path :: string()) -> ok | {error, _Reason}.
+watch(Path) -> async_server:watch(Path).
+
+-spec unwatch(Path :: string()) -> ok | {error, _Reason}.
+unwatch(Path) -> async_server:unwatch(Path).
+
+-spec pause() -> ok.
+pause() -> async_server:pause().
+-spec unpause() -> ok.
+unpause() -> async_server:unpause().
